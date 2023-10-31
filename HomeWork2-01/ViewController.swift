7341bb453c9445064e8d23f55da79c5ee5007134
Varya Utkina <vaya2412@icloud.com>
2023-10-31 21:39:11 +0300

    Initial Commit

---
+++ HomeWork2-01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork2-01
+//
+//  Created by Варвара Уткина on 31.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
